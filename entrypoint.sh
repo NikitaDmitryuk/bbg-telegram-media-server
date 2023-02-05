c0af8fd39f1a86ba8782185c810fcb8be78d11b4
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2023-02-05 23:31:22 +0300

    Refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,14 @@ architecture=armhf
 package_dir="bbg-telegram-media-server_${version}-${revision}_${architecture}"
 build_dir="build-$version"
 package_bin_dir=${package_dir}/usr/local/bin
+package_conf_dir=${package_dir}/etc
 
 sudo dpkg --add-architecture armhf
 sudo apt-get update
-sudo apt-get install -y gcc-arm-linux-gnueabihf python3-pip python3-libtorrent python3-venv python3-dev libpython3-dev:armhf zlib1g-dev:armhf
+sudo apt-get install -y python3-pip python3-libtorrent python3-venv python3-dev
 
 mkdir -p $build_dir
-ls
-cp -r application/* $build_dir
+cp -r application/. $build_dir
 
 
 python3 -m venv bbg-telegram-media-server-env
@@ -24,11 +24,13 @@ source bbg-telegram-media-server-env/bin/activate
 pip3 install python-telegram-bot cython
 
 cd $build_dir
-
-make
-
+# make
 cd ..
 
+deactivate
+
+mkdir -p ${package_conf_dir}
+cp ${build_dir}/.bbg-telegram-media-server-conf ${package_conf_dir}
 mkdir -p ${package_bin_dir}
 cp ${build_dir}/bbg-telegram-media-server ${package_bin_dir}
 mkdir -p ${package_dir}/DEBIAN
